499392b3bfbfc57ed84cd426491ac9f176e7b062
Felix Fernando <felixzhuang1@gmail.com>
2022-01-29 21:18:28 +0700

    cloudinary

--- config/plugins.js
+++ config/plugins.js
@@ -8,4 +8,18 @@ module.exports = ({ env }) => ({
         tracing: false,
       },
     },
+    upload: {
+      config: {
+        provider: 'cloudinary',
+        providerOptions: {
+          cloud_name: env('CLOUDINARY_NAME'),
+          api_key: env('CLOUDINARY_KEY'),
+          api_secret: env('CLOUDINARY_SECRET'),
+        },
+        actionOptions: {
+          upload: {},
+          delete: {},
+        },
+      },
+    },
   });
